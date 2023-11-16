801ddc03f7ef432670d3e7d07d9189d2817d4e92
Robani <muhammadrobbani77@gmail.com>
2023-11-16 22:50:26 +0700

    menerapkan blade templating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/styles.css">
-    <title>YUDIS</title>
-    <script src="https://unpkg.com/feather-icons"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet">
-</head>
-<body>
-    <nav class="navbar">
-        <a href="#" class="navbar-logo">manajemen<span>menengah</span></a>
-        <div class="navbar-nav">
-            <a href="#">beranda</a>
-            <a href="#pengumuman">pengumuman</a>
-            <a href="#raktikan">praktikan</a>
-            <a href="#praktikum">praktikum</a>
-            <a href="Profile">profile</a>
-        </div>
-        <div class="navbar-ekstra">
-            <a href="" id="hamburger-menu"><i data-feather="menu"></i></a>
-        </div>
-    </nav>
-    <!-- feather icons -->
-
-    <script>
-        feather.replace();
-    </script>
-
-    <!-- my JavaScript -->
-    <script src="js/script.js"></script>
-</body>
-</html>
+@extends('layouts.default')
+@section('content')
+    <h1>Hello World</h1>
+@endsection
---
+++ resources/views/layouts/components/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+    <a href="#" class="navbar-logo">manajemen<span>menengah</span></a>
+    <div class="navbar-nav">
+        <a href="#">beranda</a>
+        <a href="#pengumuman">pengumuman</a>
+        <a href="#raktikan">praktikan</a>
+        <a href="#praktikum">praktikum</a>
+        <a href="Profile">profile</a>
+    </div>
+    <div class="navbar-ekstra">
+        <a href="" id="hamburger-menu"><i data-feather="menu"></i></a>
+    </div>
+</nav>
