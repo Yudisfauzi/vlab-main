0511ffda934fc521440b866e838a6bbde83ff93f
Yudistiyanto Fauzi <zieedist23@gmail.com>
2023-11-29 14:03:40 +0700

    modified:   public/css/styles.css Background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.default')
 @section('content')
 <section class="home">
+    <div class="home-text">
+        <h1>chill</h1>
+        <h2>brohhhh</h2>
+    </div>
     <img class="home-img" src="/img/test.png" alt="">
 </section>
 @endsection
